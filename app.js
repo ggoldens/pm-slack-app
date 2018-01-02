5072f3b28e1fd61a4b896f70137483cb9b55dd6f
Rian Van Der Merwe <rian@wildbit.com>
2018-01-02 11:53:17 -0800

    First working prototype

--- app.js
+++ app.js
@@ -1,70 +1,70 @@
-// Import express and request modules
+// Import necessary modules
 var express = require('express');
 var request = require('request');
+var bodyParser = require('body-parser');
 
-// Store our app's ID and Secret. These we got from Step 1. 
-// For this tutorial, we'll keep your API credentials right here. But for an actual app, you'll want to  store them securely in environment variables. 
+// Store app's ID and Secret.
 var clientId = process.env.clientId;
 var clientSecret = process.env.clientSecret;
 
 // Instantiates Express and assigns our app variable to it
 var app = express();
 
-
-// Again, we define a port we want to listen to
+// Define a port we want to listen to
 const PORT=4390;
 
-// Lets start our server
+// Parse JSON and send response back
+app.use(bodyParser.json());
+
+app.post('/', function (req, res) {
+  res.send('200 Everything is ok');
+
+// Message to be sent to Slack hook
+var slackMessage = 
+ '*'
+ + req.body.Name
+ + ' received*\nThe email was sent from '
+ + req.body.From
+ + ' to '
+ + req.body.Email
+ + ', and the subject was "'
+ + req.body.Subject + '."';
+ 
+ // URL to view Bounce details in activity
+ var bounceDetailsURL = 'https://account.postmarkapp.com/servers/' + req.body.ServerID + '/messages/' + req.body.MessageID;
+ 
+ // POST to Slack hook
+request({
+	url: 'https://hooks.slack.com/services/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',
+	method: 'POST',
+	json: true,
+	body: {"text": slackMessage,
+		"attachments": [
+			{
+				"fallback": "View activity details at " + bounceDetailsURL,
+				"actions": [
+					{
+						"type": "button",
+						"name": "bounce_details",
+						"text": "View details",
+						"url": bounceDetailsURL,
+						"style": "primary"
+					}
+				]
+			}
+		]
+	},
+}, function (error, response, body){});
+
+
+})
+
+
+// Start server
 app.listen(PORT, function () {
-    //Callback triggered when server is successfully listening. Hurray!
+    //Callback triggered when server is successfully listening.
     console.log("Example app listening on port " + PORT);
 });
 
 
 	
-// POST method route
-app.post('/', function (req, res) {
-  res.send('Got a POST request')
-})
-
-/*
-
-
-// This route handles GET requests to our root ngrok address and responds with the same "Ngrok is working message" we used before
-app.get('/', function(req, res) {
-    res.send('Ngrok is working! Path Hit: ' + req.url);
-});
-
-// This route handles get request to a /oauth endpoint. We'll use this endpoint for handling the logic of the Slack oAuth process behind our app.
-app.get('/oauth', function(req, res) {
-    // When a user authorizes an app, a code query parameter is passed on the oAuth endpoint. If that code is not there, we respond with an error message
-    if (!req.query.code) {
-        res.status(500);
-        res.send({"Error": "Looks like we're not getting code."});
-        console.log("Looks like we're not getting code.");
-    } else {
-        // If it's there...
-
-        // We'll do a GET call to Slack's `oauth.access` endpoint, passing our app's client ID, client secret, and the code we just got as query parameters.
-        request({
-            url: 'https://slack.com/api/oauth.access', //URL to hit
-            qs: {code: req.query.code, client_id: clientId, client_secret: clientSecret}, //Query string data
-            method: 'GET', //Specify the method
-
-        }, function (error, response, body) {
-            if (error) {
-                console.log(error);
-            } else {
-                res.json(body);
-
-            }
-        })
-    }
-});
-
-// Route the endpoint that our slash command will point to and send back a simple response to indicate that ngrok is working
-app.post('/command', function(req, res) {
-    res.send('Your ngrok tunnel is up and running!');
-});
-
-*/
