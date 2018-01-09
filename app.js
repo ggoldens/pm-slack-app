9e93e58d56c40b30fbcaf7952830ddf61d105f2c
Rian Van Der Merwe <rian@wildbit.com>
2018-01-09 13:26:43 -0800

    Slash command changes

--- app.js
+++ app.js
@@ -20,9 +20,9 @@ var PORT = process.env.PORT || 4390;
 
 // Environment: Testing vs. Production
 //var envURL = 'http://localhost:5000/'
-var envURL = 'https://685668d7.ngrok.io/'
+//var envURL = 'https://685668d7.ngrok.io/'
 //var envURL='https://pm-slack-pr-1.herokuapp.com/'
-//var envURL='https://pm-slack.herokuapp.com/'
+var envURL='https://pm-slack.herokuapp.com/'
 
 
 // Load JSON parser
